47bf2b5a9e01302237eef4c75e2ee6911c1648b7
Edmunds <wwedmunds@gmail.com>
2023-05-25 13:20:24 +0100

    timezone list

--- src/ehs.php
+++ src/ehs.php
@@ -213,4 +213,38 @@ class ehs
 
     }
 
+    //credit to https://stackoverflow.com/users/251760/jonathan
+    function timezoneList() {
+        static $timezones = null;
+    
+        if ($timezones === null) {
+            $timezones = [];
+            $offsets = [];
+            $now = new \DateTime('now', new \DateTimeZone('UTC'));
+    
+            foreach (\DateTimeZone::listIdentifiers() as $timezone) {
+                $now->setTimezone(new \DateTimeZone($timezone));
+                $offsets[] = $offset = $now->getOffset();
+                $timezones[$timezone] = '(' . self::format_GMT_offset($offset) . ') ' . self::format_timezone_name($timezone);
+            }
+    
+            array_multisort($offsets, $timezones);
+        }
+    
+        return $timezones;
+    }
+    
+    private static function format_GMT_offset($offset) {
+        $hours = intval($offset / 3600);
+        $minutes = abs(intval($offset % 3600 / 60));
+        return 'GMT' . ($offset ? sprintf('%+03d:%02d', $hours, $minutes) : '');
+    }
+    
+    private static function format_timezone_name($name) {
+        $name = str_replace('/', ', ', $name);
+        $name = str_replace('_', ' ', $name);
+        $name = str_replace('St ', 'St. ', $name);
+        return $name;
+    }
+
 }
